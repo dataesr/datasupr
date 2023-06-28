0d7592726f3181cc191dfb67e9af64a7ae515c22
jerem <jeremy.peglion@gmail.com>
2023-06-28 16:05:32 +0200

    archi

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 
 import Layout from './layout';
 import Home from './pages/home';
+import Search from './pages/search';
+import Tableaux from './pages/tableaux';
+import EvolutionFundingSigned from './pages/tableaux/EvolutionFundingSigned';
 import Swagger from './pages/swagger';
 
 export default function Router() {
@@ -9,6 +12,12 @@ export default function Router() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+
+        <Route path="/search" element={<Search />} />
+
+        <Route path="/tableaux" element={<Tableaux />} />
+        <Route path="/tableaux/evolution-funding-signed" element={<EvolutionFundingSigned />} />
+
         <Route path="/swagger" element={<Swagger />} />
       </Route>
     </Routes>
