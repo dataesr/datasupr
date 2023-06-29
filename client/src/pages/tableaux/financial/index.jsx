1c9011e40de988a3f346993d4ed035bb70bcb708
Mihoub Debache <debache.mihoub@gmail.com>
2023-06-29 11:13:48 +0200

    fix(tableaux): grid system update

--- client/src/pages/tableaux/financial/index.jsx
+++ client/src/pages/tableaux/financial/index.jsx
@@ -7,22 +7,21 @@ import {
   BreadcrumbItem,
 } from "@dataesr/react-dsfr";
 
-import Title from "../../../components/title/index";
-
 import Histogram from "../financial/tabs/tab1";
 import Dispersion from "../financial/tabs/tab2";
+import TitleComponent from "../../../components/title";
 
 export default function Financial({ query }) {
   return (
     <Container className="fr-mb-3w">
       <Row>
         <TitleComponent as="h1" look="h1" title="Tableau de bord financier" />
-      </Row>
-      <Row>
-        <Histogram />
-      </Row>
-      <Row>
-        <Dispersion />
+        <Col n="12">
+          <Histogram />
+        </Col>
+        <Col n="12">
+          <Dispersion />
+        </Col>
       </Row>
     </Container>
   );
