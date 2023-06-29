38c14fe516efab85c86b10fa7e7e4d8179087e3b
Mihoub Debache <debache.mihoub@gmail.com>
2023-06-29 15:55:55 +0200

    update breadcrumb and add fetch and navigation logic to fiancials

--- client/src/pages/tableaux/europrojects/index.jsx
+++ client/src/pages/tableaux/europrojects/index.jsx
@@ -14,6 +14,8 @@ import RandomIdProject from "./tabs/randomIdProject";
 import ProjectSynthesis from "./tabs/projects-synthesis";
 import { useLocation } from "react-router-dom";
 import EvolutionFundingSigned from "./tabs/evolution-funding-signed";
+import graphData from "../../../assets/data/evol_all_pc_funding_SIGNED (1).json";
+
 import { useState } from "react";
 
 export default function EuropeanProjects() {
@@ -21,15 +23,15 @@ export default function EuropeanProjects() {
   const query = new URLSearchParams(location.search);
   const structureID = query.get("structureID");
   const tab = query.get("tab");
+  const data = graphData;
+  const title = data[0].label;
 
   const [selectedTab, setSelectedTab] = useState("");
-
-  console.log(structureID);
 
   const renderTab = () => {
     switch (selectedTab) {
       case "evolution-funding-signed":
-        return <EvolutionFundingSigned />;
+        return <EvolutionFundingSigned data={data} title={title} />;
         break;
 
       case "projectIdevolution":
@@ -46,8 +48,12 @@ export default function EuropeanProjects() {
     <Container>
       <Breadcrumb>
         <BreadcrumbItem href="/">Page d'accueil</BreadcrumbItem>
+        <BreadcrumbItem href="/search">Rechercher</BreadcrumbItem>
+        <BreadcrumbItem href={`/tableaux?structureId=${structureID}`}>
+          Vers les tableaux
+        </BreadcrumbItem>
       </Breadcrumb>
-      <Row className="fr-mb-3w">
+      <Row className="fr-mb-3w" gutters>
         <Col n="12">
           <TitleComponent as="h1" look="h1" title="Les projets européens" />
         </Col>
@@ -58,21 +64,33 @@ export default function EuropeanProjects() {
             <Col n="3">
               <SideMenu title="Liste de projets européens">
                 <SideMenuLink onClick={() => setSelectedTab("")}>
-                  retour à la synthese
+                  Retour à la synthese des projets européens
                 </SideMenuLink>
                 <SideMenuLink
                   onClick={() => setSelectedTab("evolution-funding-signed")}
                 >
-                  Evolution des financements signés
+                  {title}
                 </SideMenuLink>
               </SideMenu>
             </Col>
             <Col n="9">{renderTab()}</Col>
           </>
         ) : (
-          <Col n="9">
-            <RandomIdProject />
-          </Col>
+          <>
+            <Col n="3">
+              <SideMenu title={`Liste de projets européens de ${structureID}`}>
+                <SideMenuLink onClick={() => setSelectedTab("")}>
+                  Retour à la synthese de {structureID}
+                </SideMenuLink>
+                <SideMenuLink
+                  onClick={() => setSelectedTab("projectIdevolution")}
+                >
+                  Nom du tableau de {structureID} qu'on veut afficher
+                </SideMenuLink>
+              </SideMenu>
+            </Col>
+            <Col n="9">{renderTab()}</Col>
+          </>
         )}
       </Row>
     </Container>
--- client/src/pages/tableaux/financial/index.jsx
+++ client/src/pages/tableaux/financial/index.jsx
@@ -5,23 +5,89 @@ import {
   Row,
   Breadcrumb,
   BreadcrumbItem,
+  SideMenu,
+  SideMenuLink,
 } from "@dataesr/react-dsfr";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
-import Histogram from "../financial/tabs/tab1";
 import Dispersion from "../financial/tabs/tab2";
-import TitleComponent from "../../../components/title";
+import financialData from "../../../assets/data/all_treso.json";
+import FinanceGraph from "../financial/tabs/tab1";
 
-export default function Financial({ query }) {
+export default function Financial() {
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const structureID = query.get("structureID");
+  const tab = query.get("tab");
+  const data = financialData;
+  console.log(data);
+
+  const [selectedTab, setSelectedTab] = useState("");
+
+  const renderTab = () => {
+    switch (selectedTab) {
+      case "tableau-de-bord-financier":
+        return <FinanceGraph data={data} />;
+        break;
+
+      case "dispersion":
+        return <Dispersion data={data} />;
+        break;
+
+      default:
+        return (
+          <div>
+            Ici on fait un composant qui synthésise les données financières
+          </div>
+        );
+        break;
+    }
+  };
+
   return (
     <Container className="fr-mb-3w">
+      <Breadcrumb>
+        <BreadcrumbItem href="/">Page d'accueil</BreadcrumbItem>
+        <BreadcrumbItem href="/search">Rechercher</BreadcrumbItem>
+        <BreadcrumbItem href={`/tableaux?structureId=${structureID}`}>
+          Vers les tableaux
+        </BreadcrumbItem>
+      </Breadcrumb>
       <Row>
-        <TitleComponent as="h1" look="h1" title="Tableau de bord financier" />
-        <Col n="12">
-          <Histogram />
-        </Col>
-        <Col n="12">
-          <Dispersion />
-        </Col>
+        {!structureID ? (
+          <>
+            <Col n="3">
+              <SideMenu title="Tableau de bord financier">
+                <SideMenuLink onClick={() => setSelectedTab("")}>
+                  Retour à la synthese des finances
+                </SideMenuLink>
+                <SideMenuLink
+                  onClick={() => setSelectedTab("tableau-de-bord-financier")}
+                >
+                  Evolution de la trésorerie synthèse des structures/pays
+                </SideMenuLink>
+              </SideMenu>
+            </Col>
+            <Col n="9">{renderTab()}</Col>
+          </>
+        ) : (
+          <>
+            <Col n="3">
+              <SideMenu title={`Liste de projets européens de ${structureID}`}>
+                <SideMenuLink onClick={() => setSelectedTab("")}>
+                  Retour à la synthese de {structureID}
+                </SideMenuLink>
+                <SideMenuLink
+                  onClick={() => setSelectedTab("tableau-de-bord-financier")}
+                >
+                  Nom du tableau de {structureID} qu'on veut afficher
+                </SideMenuLink>
+              </SideMenu>
+            </Col>
+            <Col n="9">{renderTab()}</Col>
+          </>
+        )}
       </Row>
     </Container>
   );
