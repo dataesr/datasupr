2a3a55cfa42da30502b5709139a22bba5c50a954
jerem <jeremy.peglion@gmail.com>
2023-06-30 13:36:53 +0200

    add data

--- server/src/routes/tableaux/european-projects/index.js
+++ server/src/routes/tableaux/european-projects/index.js
@@ -2,21 +2,19 @@ import express from "express";
 
 import evol_all_pc_coordination_EVAL from './data/evol_all_pc_coordination_EVAL.json' assert { type: "json" };
 import evol_all_pc_coordination_SIGNED from './data/evol_all_pc_coordination_SIGNED.json' assert { type: "json" };
+import evol_all_pc_funding_EVAL from './data/evol_all_pc_funding_EVAL.json' assert { type: "json" };
+import evol_all_pc_funding_SIGNED from './data/evol_all_pc_funding_SIGNED.json' assert { type: "json" };
+import evol_all_pc_participant_EVAL from './data/evol_all_pc_participant_EVAL.json' assert { type: "json" };
+import evol_all_pc_participant_SIGNED from './data/evol_all_pc_participant_SIGNED.json' assert { type: "json" };
 
-// import evol_all_pc_funding_EVAL from './data/evol_all_pc_funding_EVAL.json';
-// import evol_all_pc_participant_EVAL from './data/evol_all_pc_participant_EVAL.json';
-// import evol_all_pc_participant_SIGNED from './data/evol_all_pc_participant_SIGNED.json';
-// import evol_all_pc_project_EVAL from './data/evol_all_pc_project_EVAL.json';
-// import evol_all_pc_project_SIGNED from './data/evol_all_pc_project_SIGNED.json';
-// import funding_participant_share_actions from './data/funding_participant_share_actions.json';
-// import funding_programme from './data/funding_programme.json';
+import evol_all_pc_project_EVAL from './data/evol_all_pc_project_EVAL.json' assert { type: "json" };
+import evol_all_pc_project_SIGNED from './data/evol_all_pc_project_SIGNED.json' assert { type: "json" };
+// import funding_participant_share_actions from './data/funding_participant_share_actions.json' assert { type: "json" };
+// import funding_programme from './data/funding_programme.json' assert { type: "json" };
 
-// import evol_all_pc_funding_SIGNED.json from './data/evol_all_pc_funding_SIGNED.json';
 
 
 const router = new express.Router();
-
-const allData = {};
 
 router.route('/european-projects')
     .get((req, res) => {
@@ -26,10 +24,21 @@ router.route('/european-projects')
             // filtre de toutes les data sur le pays
             allData['evol_all_pc_coordination_EVAL'] = evol_all_pc_coordination_EVAL.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
             allData['evol_all_pc_coordination_SIGNED'] = evol_all_pc_coordination_SIGNED.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
-
+            allData['evol_all_pc_funding_EVAL'] = evol_all_pc_funding_EVAL.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
+            allData['evol_all_pc_funding_SIGNED'] = evol_all_pc_funding_SIGNED.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
+            allData['evol_all_pc_participant_EVAL'] = evol_all_pc_participant_EVAL.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
+            allData['evol_all_pc_participant_SIGNED'] = evol_all_pc_participant_SIGNED.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
+            allData['evol_all_pc_project_EVAL'] = evol_all_pc_project_EVAL.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
+            allData['evol_all_pc_project_SIGNED'] = evol_all_pc_project_SIGNED.data.filter((el) => el.country_code.toLowerCase() === req.query.countryCode.toLowerCase());
         } else {
             allData['evol_all_pc_coordination_EVAL'] = evol_all_pc_coordination_EVAL.data;
             allData['evol_all_pc_coordination_SIGNED'] = evol_all_pc_coordination_SIGNED.data;
+            allData['evol_all_pc_funding_EVAL'] = evol_all_pc_funding_EVAL.data;
+            allData['evol_all_pc_funding_SIGNED'] = evol_all_pc_funding_SIGNED.data;
+            allData['evol_all_pc_participant_EVAL'] = evol_all_pc_participant_EVAL.data;
+            allData['evol_all_pc_participant_SIGNED'] = evol_all_pc_participant_SIGNED.data;
+            allData['evol_all_pc_project_EVAL'] = evol_all_pc_project_EVAL.data;
+            allData['evol_all_pc_project_SIGNED'] = evol_all_pc_project_SIGNED.data;
         }
         console.log(req.query);
 
