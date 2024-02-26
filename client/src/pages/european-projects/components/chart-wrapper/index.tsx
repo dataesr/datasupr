19c813c675b90ab6182df38c390842b8c748a8b8
jerem <jeremy.peglion@gmail.com>
2024-02-26 16:56:32 +0100

    add highlight integration code and copy button

--- client/src/pages/european-projects/components/chart-wrapper/index.tsx
+++ client/src/pages/european-projects/components/chart-wrapper/index.tsx
@@ -1,12 +1,21 @@
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { getConfig } from "../../utils";
-import { Text, Title } from "@dataesr/dsfr-plus";
+import { Button, Col, Modal, ModalContent, ModalTitle, Row, Text, Title } from "@dataesr/dsfr-plus";
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import "./styles.scss";
+import { useState } from "react";
+
 
 export default function ChartWrapper({ id, options, legend }) {
+  const [isOpen, setIsOpen] = useState(false);
   const graphConfig = getConfig(id);
+
+  const integrationCode = `<iframe \ntitle="${graphConfig.title}" \nwidth="800" \nheight="600" \nsrc="https://barometredelascienceouverte.esr.gouv.fr/integration/fr/publi.publishers.type-ouverture.chart-repartition-modeles"></iframe>`;
 
   return (
     <>
@@ -22,15 +31,49 @@ export default function ChartWrapper({ id, options, legend }) {
       </figure>
       <div className="graph-footer">
         {legend}
-        <div>
-          <p className="share">
-            Partage
-          </p>
+        <div className="share">
+          <div className="title">
+            Partager
+          </div>
+          <Button
+            color="beige-gris-galet"
+            icon="twitter-x-fill"
+            title="Twitter-X"
+            variant="text"
+          />
+          <Button title="Linkedin" icon="linkedin-box-fill" variant="text" color="beige-gris-galet" />
+          <Button title="Linkedin" icon="facebook-circle-fill" variant="text" color="beige-gris-galet" />
+          <br />
+          <Button
+            color="beige-gris-galet"
+            icon="code-s-slash-line"
+            onClick={() => setIsOpen(true)}
+            title="Intégration"
+            variant="text"
+          >
+            Intégrer
+          </Button>
         </div>
       </div>
       <Text className="description">
         {graphConfig.description}
       </Text>
+      <Modal isOpen={isOpen} hide={() => setIsOpen(false)}>
+        <ModalTitle>Intégrer ce graphique dans un autre site</ModalTitle>
+        <ModalContent>
+          <Row>
+            <Col className="text-right">
+              <CopyToClipboard text={integrationCode}>
+                <Button size="sm" icon="clipboard-line" color="beige-gris-galet">Copier</Button>
+              </CopyToClipboard>
+            </Col>
+          </Row>
+
+          <SyntaxHighlighter language="javascript" style={a11yDark}>
+            {integrationCode}
+          </SyntaxHighlighter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
