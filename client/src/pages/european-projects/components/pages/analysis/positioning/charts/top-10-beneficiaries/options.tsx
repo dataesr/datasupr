574379b8da845bd47563c4501def2b9a3df14956
jerem <jeremy.peglion@gmail.com>
2024-03-08 10:14:24 +0100

    review fix

--- client/src/pages/european-projects/components/pages/analysis/positioning/charts/top-10-beneficiaries/options.tsx
+++ client/src/pages/european-projects/components/pages/analysis/positioning/charts/top-10-beneficiaries/options.tsx
@@ -36,13 +36,11 @@ export default function Options(data, country_code) {
       name: 'Total subventions en euros',
       type: "column",
       colorByPoint: true,
-      data: data.top10.map((item) => {
-        return {
-          name: item.name_fr,
-          y: item.total_fund_eur,
-          color: (item.id == country_code) ? '#6E445A' : '#233E41'
-        }
-      }),
+      data: data.top10.map((item) => ({
+        name: item.name_fr,
+        y: item.total_fund_eur,
+        color: (item.id == country_code) ? '#6E445A' : '#233E41'
+      })),
       tooltip: {
         pointFormat: 'Total des subventions : <b>{point.y:.1f} €</b>'
       },
