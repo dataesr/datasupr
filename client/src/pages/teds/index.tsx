a5d05e30871238a9804de668d900ae86260c8707
hfsllt <100839261+hfsllt@users.noreply.github.com>
2024-04-19 10:23:07 +0200

    Teds interfaces (#22)
    * feat(teds): creation of highcharts series
    
    * feat(teds): plot graph of interfaces
    
    * feat(teds): prettier code
    
    * feat(teds): re write code
    
    * feat(teds): space between imports
    
    * feat(teds): update on values ( directly from filters)
    
    * feat(teds): h1 into h2 for titles
    
    * feat(teds): 2 if into 1 if - isLoading
    
    * feat(teds): remove the function isLoad
    
    ---------
    
    Co-authored-by: hfsllt <hafsa.lt21@gmail.com>

--- client/src/pages/teds/index.tsx
+++ client/src/pages/teds/index.tsx
@@ -1,47 +1,75 @@
-import { Col, Container, Row, Title } from '@dataesr/dsfr-plus';
-import PrctIpccPublicationsByCountry from './charts/PrctIpccPublicationsByCountry';
-import PrctIpbesPublicationsByCountry from './charts/PrctIpbesPublicationsByCountry';
-import PrctIpccPublicationsByCountryByWg1 from './charts/PrctIpccPublicationsByCountryByWg1';
-import PrctIpccPublicationsByCountryByWg2 from './charts/PrctIpccPublicationsByCountryByWg2';
-import PrctIpccPublicationsByCountryByWg2cross from './charts/PrctIpccPublicationsByCountryByWg2cross';
-import PrctIpccPublicationsByCountryByWg3 from './charts/PrctIpccPublicationsByCountryByWg3';
+import { Col, Container, Row, Title } from "@dataesr/dsfr-plus";
+
+import PrctIpccPublicationsByCountry from "./charts/PrctIpccPublicationsByCountry";
+import PrctIpbesPublicationsByCountry from "./charts/PrctIpbesPublicationsByCountry";
+import PrctIpccPublicationsByCountryByWg1 from "./charts/PrctIpccPublicationsByCountryByWg1";
+import PrctIpccPublicationsByCountryByWg2 from "./charts/PrctIpccPublicationsByCountryByWg2";
+import PrctIpccPublicationsByCountryByWg2cross from "./charts/PrctIpccPublicationsByCountryByWg2cross";
+import PrctIpccPublicationsByCountryByWg3 from "./charts/PrctIpccPublicationsByCountryByWg3";
+import PrctIpccPublicationsForFiveCountries from "./charts/PrctIpccPublicationsForFiveCountries";
 
 export default function Welcome() {
   return (
     <>
-    <Container>
-    <Title as="h2" className="fr-mb-2w">
-      Part de publications pour le rapport du GIEC par pays (top 20)
-    </Title>
-      <PrctIpccPublicationsByCountry />
-    </Container>
-    <Container>
-      <Title as="h2" className="fr-mb-2w">
-        Part de publications pour les différents groupes du rapport du GIEC par pays (top 10)
-      </Title>
-      <Row>
-        <Col>
-          <PrctIpccPublicationsByCountryByWg1 />
-        </Col>
-        <Col>
-          <PrctIpccPublicationsByCountryByWg2 />
-        </Col>
-      </Row>
-      <Row>
-      <Col>
-        <PrctIpccPublicationsByCountryByWg2cross />
-      </Col>
-      <Col>
-        <PrctIpccPublicationsByCountryByWg3 />
-      </Col>
-      </Row>
-    </Container>
-    <Container>
-      <Title as="h2" className="fr-mb-2w">
-        Part de publications pour le rapport de l'IPBES par pays (top 20)
-      </Title>
-      <PrctIpbesPublicationsByCountry />
-    </Container>
+      <Container as="main">
+        <Title as="h1" className="fr-mb-2w">
+          Transition écologique pour un développement soutenable
+        </Title>
+        <Title as="h2" className="fr-mb-2w">
+          Rapport du GIEC
+        </Title>
+        <Row gutters>
+          <Col md={12}>
+            <Title as="h3" className="fr-mb-2w">
+              Part de publications pour le rapport du GIEC par pays (top 20)
+            </Title>
+            <PrctIpccPublicationsByCountry />
+          </Col>
+        </Row>
+        <Title as="h3" className="fr-mb-2w">
+          Part de publications pour les différents groupes du rapport du GIEC
+          par pays (top 10)
+        </Title>
+        <Row>
+          <Col md={6}>
+            <PrctIpccPublicationsByCountryByWg1 />
+          </Col>
+          <Col md={6}>
+            <PrctIpccPublicationsByCountryByWg2 />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <PrctIpccPublicationsByCountryByWg2cross />
+          </Col>
+          <Col md={6}>
+            <PrctIpccPublicationsByCountryByWg3 />
+          </Col>
+        </Row>
+        <Row gutters>
+          <Col md={12}>
+            <Title as="h3" className="fr-mb-2w">
+              Structures des contribitions pour cinq pays
+            </Title>
+            <PrctIpccPublicationsForFiveCountries />
+          </Col>
+        </Row>
+        <Row gutters>
+          <Col md={12}>
+            <Title as="h2" className="fr-mb-2w">
+              Rapport de l'IPBES
+            </Title>
+          </Col>
+        </Row>
+        <Row gutters>
+          <Col md={12}>
+            <Title as="h3" className="fr-mb-2w">
+              Part de publications pour le rapport de l'IPBES par pays (top 20)
+            </Title>
+            <PrctIpbesPublicationsByCountry />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
