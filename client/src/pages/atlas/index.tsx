67f6c4c7cdfeb83213599d19abf97bdc843a0977
jerem <jeremy.peglion@gmail.com>
2024-06-11 10:54:23 +0200

    add colorized badge in title

--- client/src/pages/atlas/index.tsx
+++ client/src/pages/atlas/index.tsx
@@ -1,41 +1,54 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import { useQuery } from '@tanstack/react-query';
-import { Badge, Button, Breadcrumb, Container, Row, Col, Link } from '@dataesr/dsfr-plus';
-import { useTitle } from '../../hooks/usePageTitle.tsx';
+import { useQuery } from "@tanstack/react-query";
+import {
+  Badge,
+  Button,
+  Breadcrumb,
+  Container,
+  Row,
+  Col,
+  Link,
+} from "@dataesr/dsfr-plus";
+import { useTitle } from "../../hooks/usePageTitle.tsx";
 
-import { getFiltersValues, getParentsFromGeoId } from '../../api/atlas.ts';
-import { getGeoLabel, getParentFromLevel, setfavoriteIdsInCookie } from '../../utils.tsx';
-import { Search } from './components/main/tabs/search/index.tsx';
+import { getFiltersValues, getParentsFromGeoId } from "../../api/atlas.ts";
+import {
+  getGeoLabel,
+  getParentFromLevel,
+  setfavoriteIdsInCookie,
+} from "../../utils.tsx";
+import { Search } from "./components/main/tabs/search/index.tsx";
 // import Header from './components/main/header/index.tsx';
-import YearsModalButton from './components/main/header/years-modal-button.tsx';
+import YearsModalButton from "./components/main/header/years-modal-button.tsx";
 
-import './styles.scss';
-import { AtlasSideMenu } from './side-menu-layout/index.tsx';
+import "./styles.scss";
+import { AtlasSideMenu } from "./side-menu-layout/index.tsx";
+import { GetLevelBadgeFromId } from "./utils/badges.tsx";
 
 export default function AtlasHeader() {
   const [searchParams] = useSearchParams();
-  const geoId = searchParams.get('geo_id') || '';
-  const currentYear = searchParams.get('annee_universitaire') || '2022-23';
+  const geoId = searchParams.get("geo_id") || "";
+  const currentYear = searchParams.get("annee_universitaire") || "2022-23";
   const navigate = useNavigate();
 
-  useTitle('dataSupR - Atlas des effectifs étudiant-e-s');
+  useTitle("dataSupR - Atlas des effectifs étudiant-e-s");
 
   useEffect(() => {
-    if (searchParams.get('geo_id')) {
-      setfavoriteIdsInCookie(searchParams.get('geo_id') || '');
+    if (searchParams.get("geo_id")) {
+      setfavoriteIdsInCookie(searchParams.get("geo_id") || "");
     }
   }, [searchParams]);
 
   const { data: filtersValues, isLoading: isLoadingFiltersValues } = useQuery({
     queryKey: ["atlas/get-filters-values", geoId],
-    queryFn: () => getFiltersValues(geoId)
-  })
+    queryFn: () => getFiltersValues(geoId),
+  });
 
   const { data: dataParents, isLoading: isLoadingParents } = useQuery({
     queryKey: ["atlas/get-parents-from-geoId", geoId],
-    queryFn: () => getParentsFromGeoId(geoId)
-  })
+    queryFn: () => getParentsFromGeoId(geoId),
+  });
 
   let parent;
   if (!isLoadingParents) {
@@ -54,57 +67,48 @@ export default function AtlasHeader() {
     );
   }
 
-  let geoLabelFull;
   const geoLabel = getGeoLabel(geoId, filtersValues?.geo_id);
-  switch (geoId?.charAt(0)) {
-    case 'R':
-      geoLabelFull = `Région ${geoLabel}`;
-      break;
-    case 'D':
-      geoLabelFull = `Département "${geoLabel}"`;
-      break;
-    case 'A':
-      geoLabelFull = `Académie "${geoLabel}"`;
-      break;
-    case 'U':
-      geoLabelFull = <>{geoLabel} <Badge>Unité urbaine</Badge></>;
-      break;
-  }
+  const geoLabelFull = (
+    <>
+      {geoLabel}
+      {GetLevelBadgeFromId({ id: geoId })}
+    </>
+  );
 
-  document.documentElement.setAttribute('data-fr-scheme', 'light');
+  document.documentElement.setAttribute("data-fr-scheme", "light");
 
   return (
     <Container as="main" className="atlas-header">
       <Row>
         <Col>
-          {
-            geoId && (
-              <Button
-                className="button"
-                color="pink-tuile"
-                icon='home-4-line'
-                onClick={() => navigate('/atlas')}
-                size="sm"
-              >
-                Revenir à la page de sélection des territoires
-              </Button>
-            )
-          }
-          {
-            geoId && !isLoadingParents && dataParents && parent && (
-              <Button
-                className="button"
-                color="pink-tuile"
-                icon='arrow-up-line'
-                onClick={() => navigate(`/atlas/general?geo_id=${parent.geo_id}&annee_universitaire=${currentYear}`)}
-                size="sm"
-              >
-                Revenir au territoire parent ({parent.geo_nom})
-              </Button>
-            )
-          }
+          {geoId && (
+            <Button
+              className="button"
+              color="pink-tuile"
+              icon="home-4-line"
+              onClick={() => navigate("/atlas")}
+              size="sm"
+            >
+              Revenir à la page de sélection des territoires
+            </Button>
+          )}
+          {geoId && !isLoadingParents && dataParents && parent && (
+            <Button
+              className="button"
+              color="pink-tuile"
+              icon="arrow-up-line"
+              onClick={() =>
+                navigate(
+                  `/atlas/general?geo_id=${parent.geo_id}&annee_universitaire=${currentYear}`
+                )
+              }
+              size="sm"
+            >
+              Revenir au territoire parent ({parent.geo_nom})
+            </Button>
+          )}
         </Col>
-        <Col md={3} style={{ textAlign: 'right' }}>
+        <Col md={3} style={{ textAlign: "right" }}>
           <YearsModalButton />
         </Col>
       </Row>
@@ -112,12 +116,10 @@ export default function AtlasHeader() {
       <Breadcrumb>
         <Link href="/">Accueil</Link>
         <Link href="/atlas">Atlas des effectifs étudiant-e-s</Link>
-        {
-          geoId && <Link>{geoLabelFull}</Link>
-        }
+        {geoId && <Link>{geoLabel}</Link>}
       </Breadcrumb>
 
-      {(!geoId) ? <Search /> : <AtlasSideMenu title={geoLabelFull} />}
-    </Container >
-  )
+      {!geoId ? <Search /> : <AtlasSideMenu title={geoLabelFull} />}
+    </Container>
+  );
 }
--- client/src/utils.tsx
+++ client/src/utils.tsx
@@ -1,63 +1,64 @@
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 type Geo = {
-  geo_id: string,
-  geo_nom: string,
-}
+  geo_id: string;
+  geo_nom: string;
+};
 
 type GeoData = {
-  regions: Geo[],
-  departements: Geo[],
-  academies: Geo[],
-  unites_urbaines: Geo[],
-  communes: Geo[],
-}
+  regions: Geo[];
+  departements: Geo[];
+  academies: Geo[];
+  unites_urbaines: Geo[];
+  communes: Geo[];
+};
 
 function getGeoLabel(needle: string, haystack: GeoData) {
   if (!haystack || !needle) {
-    return 'vide';
+    return "vide";
   }
 
   let newHaystack: Geo[] = [];
 
   switch (needle.charAt(0)) {
-    case 'R':
+    case "R":
       newHaystack = haystack.regions;
       break;
-    case 'D':
+    case "D":
       newHaystack = haystack.departements;
       break;
-    case 'A':
+    case "A":
       newHaystack = haystack.academies;
       break;
-    case 'U':
+    case "U":
       newHaystack = haystack.unites_urbaines;
       break;
     default:
       newHaystack = haystack.communes;
   }
 
-  const geo = newHaystack.find((item) => item.geo_id === needle)
-  return geo ? geo.geo_nom : ''
+  const geo = newHaystack.find((item) => item.geo_id === needle);
+
+  return geo?.geo_nom || "France";
 }
 
 function getParentFromLevel(parents, geoId) {
   if (!parents) {
     return null;
   }
-  if (geoId === 'PAYS_100') {
+  if (geoId === "PAYS_100") {
     return null;
   }
-  if (geoId.startsWith('R')) {
-    return { geo_nom: 'France', geo_id: 'PAYS_100' };
+  if (geoId.startsWith("R")) {
+    return { geo_nom: "France", geo_id: "PAYS_100" };
   }
-  if (geoId.startsWith('A')) {
+  if (geoId.startsWith("A")) {
     return { geo_nom: parents.reg_nom, geo_id: parents.reg_id };
   }
-  if (geoId.startsWith('D')) {
+  if (geoId.startsWith("D")) {
     return { geo_nom: parents.aca_nom, geo_id: parents.aca_id };
   }
-  if (geoId.startsWith('U')) {
+  if (geoId.startsWith("U")) {
     return { geo_nom: parents.reg_nom, geo_id: parents.reg_id };
   }
   return { geo_nom: parents.dep_nom, geo_id: parents.dep_id };
@@ -65,9 +66,9 @@ function getParentFromLevel(parents, geoId) {
 
 const getfavoriteIdsInCookie = () => {
   if (Cookies.get("favoriteIds")) {
-    return Cookies.get("favoriteIds") || '';
+    return Cookies.get("favoriteIds") || "";
   }
-  return '';
+  return "";
 };
 
 const getSortedfavoriteIdsInCookie = () => {
@@ -75,26 +76,28 @@ const getSortedfavoriteIdsInCookie = () => {
   // count number of items of each id
   // sort by number of items
   const favoriteIdsCount = {};
-  favoriteIds.split(',').forEach(element => {
+  favoriteIds.split(",").forEach((element) => {
     if (favoriteIdsCount[element]) {
       favoriteIdsCount[element] += 1;
     } else {
       favoriteIdsCount[element] = 1;
     }
   });
-  const sortedfavoriteIds = Object.keys(favoriteIdsCount).sort((a, b) => favoriteIdsCount[b] - favoriteIdsCount[a]);
+  const sortedfavoriteIds = Object.keys(favoriteIdsCount).sort(
+    (a, b) => favoriteIdsCount[b] - favoriteIdsCount[a]
+  );
   return sortedfavoriteIds;
-}
+};
 
 const clearAllfavoriteIdsInCookie = () => {
-  Cookies.remove('favoriteIds');
+  Cookies.remove("favoriteIds");
 };
 
 const setfavoriteIdsInCookie = (id: string) => {
   const oldCookie = getfavoriteIdsInCookie();
   clearAllfavoriteIdsInCookie();
-  const newCookie = (oldCookie === '') ? id : (oldCookie + ',' + id);
-  Cookies.set('favoriteIds', newCookie);
+  const newCookie = oldCookie === "" ? id : oldCookie + "," + id;
+  Cookies.set("favoriteIds", newCookie);
 };
 
 export {
