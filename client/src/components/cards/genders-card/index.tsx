37b942233fb48071fd024426ff6682c7d87ecdda
jerem <jeremy.peglion@gmail.com>
2024-06-11 14:45:25 +0200

    improve style

--- client/src/components/cards/genders-card/index.tsx
+++ client/src/components/cards/genders-card/index.tsx
@@ -2,13 +2,13 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 type valuesProps = {
-  labels: string[],
-  values: number[],
+  labels: string[];
+  values: number[];
 };
 
 type CardProps = {
-  currentYear: string,
-  values: valuesProps,
+  currentYear: string;
+  values: valuesProps;
 };
 
 import "./style.scss";
@@ -19,56 +19,52 @@ export default function GendersCard({ currentYear, values }: CardProps) {
       plotBackgroundColor: null,
       plotBorderWidth: 0,
       plotShadow: false,
-      height: '60%',
+      height: "60%",
     },
     title: {
-      text: 'Genres',
-      align: 'center',
-      verticalAlign: 'middle',
-      y: 60
+      text: "Genres",
+      align: "center",
+      verticalAlign: "middle",
+      y: 60,
     },
     tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
     },
     accessibility: {
       point: {
-        valueSuffix: '%'
-      }
+        valueSuffix: "%",
+      },
     },
     plotOptions: {
       pie: {
-        dataLabels: {
-          enabled: true,
-          distance: -50,
-          style: {
-            fontWeight: 'bold',
-            color: 'white'
-          }
-        },
+        dataLabels: { enabled: false },
         startAngle: -90,
         endAngle: 90,
-        center: ['50%', '75%'],
-        size: '110%'
-      }
+        center: ["50%", "75%"],
+        size: "150%",
+      },
     },
     credits: {
-      enabled: false
+      enabled: false,
     },
-    colors: ['#efcb3a', '#e18b76'],
-    series: [{
-      type: 'pie',
-      name: currentYear,
-      innerSize: '50%',
-      data: values.labels.map((label, index) => [label, values.values[index]])
-    }]
-  }
+    colors: ["#efcb3a", "#e18b76"],
+    series: [
+      {
+        type: "pie",
+        name: currentYear,
+        innerSize: "10%",
+        data: values.labels.map((label, index) => [
+          label,
+          values.values[index],
+        ]),
+        showInLegend: true,
+      },
+    ],
+  };
 
   return (
     <figure>
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={secteursOptions}
-      />
+      <HighchartsReact highcharts={Highcharts} options={secteursOptions} />
     </figure>
   );
 }
