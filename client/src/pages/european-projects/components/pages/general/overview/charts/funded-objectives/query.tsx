0bf59e894545631b41b1b7e8f95d77674cf84384
jerem <jeremy.peglion@gmail.com>
2024-09-17 09:12:08 +0200

    fix

--- client/src/pages/european-projects/components/pages/general/overview/charts/funded-objectives/query.tsx
+++ client/src/pages/european-projects/components/pages/general/overview/charts/funded-objectives/query.tsx
@@ -1,11 +1,23 @@
 const { VITE_APP_SERVER_URL } = import.meta.env;
 
 export async function GetfundedObjectives(params: string) {
+  console.log("GetfundedObjectives", params);
+  const newParamsObj = JSON.parse(
+    '{"' +
+      decodeURI(params)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"') +
+      '"}'
+  );
+  delete newParamsObj.entities;
+  const newParams = new URLSearchParams(newParamsObj).toString();
+  // TODO: Implement parameter entities ?
+
   let url = `${VITE_APP_SERVER_URL}/european-projects/analysis-synthese-funding_programme`;
-  if (params !== '') {
-    url = `${VITE_APP_SERVER_URL}/european-projects/analysis-synthese-funding_programme?${params}`;
+  if (params !== "") {
+    url = `${VITE_APP_SERVER_URL}/european-projects/analysis-synthese-funding_programme?${newParams}`;
   }
 
-  return fetch(url).then((response) => (response.json()))
+  return fetch(url).then((response) => response.json());
 }
-
