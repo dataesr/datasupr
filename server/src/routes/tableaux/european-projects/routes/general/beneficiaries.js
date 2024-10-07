527dc964946d3db48f470802a2cd9d3ef14c2cf5
jerem <jeremy.peglion@gmail.com>
2024-10-07 11:43:59 +0200

    fix

---
+++ server/src/routes/tableaux/european-projects/routes/general/beneficiaries.js
@@ -0,0 +1,7 @@
+import express from "express";
+// import { db } from "../../../../../services/mongo.js";
+// import { checkQuery } from "../../utils";
+
+const router = new express.Router();
+
+export default router;
