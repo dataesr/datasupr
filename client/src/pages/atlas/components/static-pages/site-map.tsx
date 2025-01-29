bc5f676ed5ef658ca4ba3da6ce20a90d6a401d7e
mialy <rakoto.mk@gmail.com>
2025-01-29 17:01:51 +0100

    feat(sitemap): update links

--- client/src/pages/atlas/components/static-pages/site-map.tsx
+++ client/src/pages/atlas/components/static-pages/site-map.tsx
@@ -6,7 +6,10 @@ import {
   Link,
 } from "@dataesr/dsfr-plus";
 
+import { DEFAULT_CURRENT_YEAR, DEFAULT_GEO_ID } from "../../../../constants.tsx";
+
 export default function SiteMap() {
+    
   return (
       <Container as="main" id="main">
         <Row className="fr-mb-5w">
@@ -29,60 +32,60 @@ export default function SiteMap() {
             </li>
             <ul className="fr-my-1" style={{ listStyleType: "disc", paddingLeft: "1em" }}>
               <li>
-                <Link href="/atlas/general?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Vue générale des effectis étudiant·e·s</Link>
+                <Link href={`/atlas/general?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>En un coup d'oeil</Link>
               </li>
               <li>
-                <Link href="/atlas/effectifs-par-filiere?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Vue générale des filières</Link>
+                <Link href={`/atlas/effectifs-par-filiere?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs par filière</Link>
               </li>
               <ul className="fr-my-1" style={{ listStyleType: "disc", paddingLeft: "1em" }}>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/CPGE?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des classes préparatoires aux grandes écoles</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/CPGE?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des classes préparatoires aux grandes écoles</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/STS?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des sections de techniciens supérieurs et assimilés</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/STS?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des sections de techniciens supérieurs et assimilés</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/UNIV?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des universités</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/UNIV?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des universités</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/GE?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des grands établissements MESR</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/GE?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des grands établissements MESR</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/UT?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des universités de technologie</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/UT?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des universités de technologie</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/INP?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des instituts nationaux polytechniques</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/INP?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des instituts nationaux polytechniques</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/ENS?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des écoles normales supérieures</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/ENS?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des écoles normales supérieures</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/EPEU?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des établissements d'enseignement universitaire privés</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/EPEU?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des établissements d'enseignement universitaire privés</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/ING_autres?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des autres formations d'ingénieurs</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/ING_autres?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des autres formations d'ingénieurs</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/EC_COM?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des écoles de commerce, gestion et comptabilité</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/EC_COM?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des écoles de commerce, gestion et comptabilité</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/EC_JUR?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des écoles juridiques et administratives</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/EC_JUR?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des écoles juridiques et administratives</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/EC_ART?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des écoles supérieures art et culture</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/EC_ART?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des écoles supérieures art et culture</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/EC_PARAM?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des écoles paramédicales et sociales</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/EC_PARAM?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des écoles paramédicales et sociales</Link>
                 </li>
                 <li>
-                  <Link href="/atlas/effectifs-par-filiere/EC_autres?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Effectifs des autres écoles de spécialités diverses</Link>
+                  <Link href={`/atlas/effectifs-par-filiere/EC_autres?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs des autres écoles de spécialités diverses</Link>
                 </li>
               </ul>
               <li>
-                <Link href="/atlas/effectifs-par-secteurs?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Vue générale : secteurs</Link>
+                <Link href={`/atlas/effectifs-par-secteurs?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs par secteurs</Link>
               </li>
               <li>
-                <Link href="/atlas/effectifs-par-genre?geo_id=PAYS_100&annee_universitaire=2023-24&datasupr=true">Vue générale : genre</Link>
+                <Link href={`/atlas/effectifs-par-genre?geo_id=${DEFAULT_GEO_ID}&annee_universitaire=${DEFAULT_CURRENT_YEAR}&datasupr=true`}>Effectifs par genre</Link>
               </li>
             </ul>
             <li>
