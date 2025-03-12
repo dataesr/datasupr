24b7ef6c09585a3ee7bdb130e97da3743c1cb225
jerem <jeremy.peglion@gmail.com>
2025-03-12 13:50:38 +0100

    fix(vite): mise à jour de la configuration pour utiliser le compilateur moderne avec Sass

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    // TODO : remove this block - https://sass-lang.com/documentation/breaking-changes/legacy-js-api/
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler", // or "modern"
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern',
+        },
       },
     },
-  },
 });
