6b404377fa5d882d41c8763685f5d7418c6110a4
jerem <jeremy.peglion@gmail.com>
2025-04-23 15:29:27 +0200

    feat(ep): ajout des graphiques pour les valeurs de financement, les taux de succès et les proportions des programmes

---
+++ client/src/boards/european-projects/pages/overview/charts/programs-funding-success-rates/options.tsx
@@ -0,0 +1,57 @@
+import HighchartsInstance from "highcharts";
+
+import { CreateChartOptions } from "../../../../components/chart-ep";
+import { formatToRates } from "../../../../../../utils/format";
+
+export default function Options(data) {
+  if (!data) return null;
+
+  const rootStyles = getComputedStyle(document.documentElement);
+
+  const newOptions: HighchartsInstance.Options = {
+    legend: { enabled: true },
+    xAxis: {
+      labels: {
+        enabled: false,
+      },
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "Taux de succès",
+      },
+    },
+    tooltip: {
+      pointFormatter: function () {
+        return `Taux de succès : <b>${formatToRates(this.y ?? 0)}</b>`;
+      },
+    },
+    plotOptions: {
+      series: {
+        dataLabels: { enabled: true },
+      },
+      bar: {
+        pointWidth: 50,
+      },
+    },
+    series: [
+      {
+        type: "bar",
+        name: "Taux de succès",
+        color: rootStyles.getPropertyValue("--successRate-color"),
+        groupPadding: 0,
+        data: data.successRateByProgram.map((item) => [
+          item.pillar,
+          item.successRate,
+        ]),
+        dataLabels: {
+          formatter: function () {
+            return formatToRates(this.y ?? 0);
+          },
+        },
+      },
+    ],
+  };
+
+  return CreateChartOptions("bar", newOptions);
+}
--- client/src/boards/european-projects/pages/overview/components/programs-funding.tsx
+++ client/src/boards/european-projects/pages/overview/components/programs-funding.tsx
@@ -1,25 +1,22 @@
 import { Col, Row } from "@dataesr/dsfr-plus";
-// import DestinationFundingValues from "../charts/destination-funding";
-// import DestinationFundingSuccessRates from "../charts/destination-funding-success-rates";
-// import DestinationFundingProportion from "../charts/destination-funding-proportion";
+import ProgramsFundingValues from "../charts/programs-funding";
+import ProgramsFundingSuccessRates from "../charts/programs-funding-success-rates";
+import ProgramsFundingProportion from "../charts/programs-funding-proportion";
 
 export default function ProgramsFunding() {
   return (
     <>
       <Row gutters>
         <Col>
-          {/* <DestinationFundingValues /> */}
-          graph1
+          <ProgramsFundingValues />
         </Col>
         <Col>
-          {/* <DestinationFundingSuccessRates /> */}
-          graph2
+          <ProgramsFundingSuccessRates />
         </Col>
       </Row>
       <Row>
         <Col>
-          {/* <DestinationFundingProportion /> */}
-          graph3
+          <ProgramsFundingProportion />
         </Col>
       </Row>
     </>
