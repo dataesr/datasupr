39447c5c58988087892741798c0f6c408fe57207
Mihoub <debache.mihoub@gmail.com>
2025-05-26 15:00:58 +0200

    clean code

--- client/src/boards/faculty-members/utils.tsx
+++
@@ -1,9 +0,0 @@
-import config from "./charts-config.json";
-
-export function getConfig(id) {
-  const chartConfig = config.find((item) => item.id === id);
-  if (!chartConfig) {
-    throw new Error(`No config found for chart id ${id}`);
-  }
-  return chartConfig;
-}
