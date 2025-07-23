39ccd57f8514f202c77913e21c3b5e33bcad35d0
jerem <jeremy.peglion@gmail.com>
2025-07-23 17:24:23 +0200

    fix(top-10-beneficiaries): corriger l'URL d'intégration pour éviter les erreurs. String attendu

--- client/src/boards/european-projects/pages/type-of-beneficiaries/charts/top-10-beneficiaries/index.tsx
+++ client/src/boards/european-projects/pages/type-of-beneficiaries/charts/top-10-beneficiaries/index.tsx
@@ -41,7 +41,7 @@ const config = {
     fr: "Ad duis occaecat voluptate deserunt tempor enim nulla officia.",
     en: "Ad duis occaecat voluptate deserunt tempor enim nulla officia.",
   },
-  integrationURL: null,
+  integrationURL: "",
 };
 // top10-countries-by-type-of-beneficiaries
 export default function Top10CountriesByTypeOfBeneficiaries() {
