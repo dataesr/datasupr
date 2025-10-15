9fec7ebee73bb1fa2f9a76753cf84d53f4b20cd6
Mihoub <debache.mihoub@gmail.com>
2025-10-15 10:15:45 +0200

    fix(faculty-members): test env

--- client/src/boards/faculty-members/api/general-queries.tsx
+++ client/src/boards/faculty-members/api/general-queries.tsx
@@ -4,7 +4,7 @@ const { VITE_APP_SERVER_URL } = import.meta.env;
 
 export const useFacultyMembersYears = () => {
   const [searchParams] = useSearchParams();
-
+  console.log(VITE_APP_SERVER_URL);
   const structure_id = searchParams.get("structure_id");
   const field_id = searchParams.get("field_id");
   const geo_id = searchParams.get("geo_id");
@@ -17,15 +17,18 @@ export const useFacultyMembersYears = () => {
   return useQuery({
     queryKey,
     queryFn: async () => {
-      const url = new URL(
-        `${VITE_APP_SERVER_URL}/faculty-members/filters/years`
-      );
+      let url = `${VITE_APP_SERVER_URL}/faculty-members/filters/years`;
 
-      if (structure_id) url.searchParams.append("structure_id", structure_id);
-      if (field_id) url.searchParams.append("field_id", field_id);
-      if (geo_id) url.searchParams.append("geo_id", geo_id);
+      const params = new URLSearchParams();
+      if (structure_id) params.append("structure_id", structure_id);
+      if (field_id) params.append("field_id", field_id);
+      if (geo_id) params.append("geo_id", geo_id);
 
-      const response = await fetch(url.toString());
+      if (params.toString()) {
+        url += `?${params.toString()}`;
+      }
+
+      const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Erreur lors de la récupération des années");
       }
