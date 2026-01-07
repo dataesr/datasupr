f6790f8011883a0a7ff1ce4bfbe6976740f1f600
jerem <jeremy.peglion@gmail.com>
2026-01-07 17:05:35 +0100

    feat(overview): Ajout de nouveaux composants et exemples dans la documentation

--- client/src/boards/datasupr-doc/pages/overview/components/id4-components/index.tsx
+++ client/src/boards/datasupr-doc/pages/overview/components/id4-components/index.tsx
@@ -1,5 +1,8 @@
 import { Row, Col, Title, Badge } from "@dataesr/dsfr-plus";
 import Callout from "../../../../../../components/callout";
+import CopyButton from "../../../../../../components/copy-button";
+import CountrySelector from "../../../../../../components/country-selector/selector";
+import FieldsMainCard from "../../../../../../components/cards/fields-main-card";
 
 export default function Id4Components() {
   return (
@@ -11,37 +14,84 @@ export default function Id4Components() {
           </Title>
           <Callout>
             Voici la liste des composants réutilisables disponibles dans la bibliothèque de composants Datasupr&nbsp;:
-            <ul>
+            <ul className="components-list">
               <li>
                 <Badge color="purple-glycine">BoardSuggestComponent</Badge>&nbsp;: composant de suggestion de tableaux de bord.
+                <code>{`<BoardsSuggestComponent />`}</code>
               </li>
               <li>
                 <Badge color="purple-glycine">Callout</Badge>&nbsp;: encadré d'information avec style visuel distinct.
+                <Callout className="fr-mt-1w" colorFamily="blue-cumulus">
+                  Exemple de Callout imbriqué pour démontrer la réutilisabilité du composant Callout à l'intérieur d'un autre Callout.
+                </Callout>
+                <code>
+                  {`<Callout>
+  Exemple de Callout imbriqué pour démontrer la réutilisabilité du composant Callout à l'intérieur d'un autre Callout.
+</Callout>`}
+                </code>
               </li>
               <li>
                 <Badge color="purple-glycine">ChartFooter</Badge>&nbsp;: pied de page des graphiques avec informations de source et date.
+                <code>
+                  {`<ChartFooter
+  comment={{ fr: <>Commentaire en français</>, en: <>Commentaire en anglais</> }}
+  readingKey={{ fr: <>Clé de lecture en français</>, en: <>Clé de lecture en anglais</> }}
+  source=Source des données
+  updateDate=Date de mise à jour
+/>`}
+                </code>
               </li>
               <li>
                 <Badge color="purple-glycine">ChartWrapper</Badge>&nbsp;: composant d'encapsulation des graphiques avec gestion des options, de la
                 légende et du rendu tableau.
-              </li>
-              <li>
-                <Badge color="purple-glycine">ChartsSkeleton</Badge>&nbsp;: squelettes de chargement pour les graphiques.
+                <br />
+                <code>
+                  {`<ChartWrapper
+  config={config}
+  legend={null}
+  options={options(data, currentLang)}
+  renderData={() => renderDataTable(data, currentLang)}
+/>`}
+                </code>
               </li>
               <li>
                 <Badge color="purple-glycine">Cookies</Badge>&nbsp;: composant de gestion des cookies et consentement.
               </li>
               <li>
                 <Badge color="purple-glycine">CopyButton</Badge>&nbsp;: bouton de copie dans le presse-papier.
+                <br />
+                <CopyButton text="Texte à copier" />
+                <br />
+                <code>{`<CopyButton textToCopy="Texte à copier" />`}</code>
               </li>
               <li>
                 <Badge color="purple-glycine">CountrySelector</Badge>&nbsp;: sélecteur de pays avec recherche.
+                <br />
+                <CountrySelector />
+                <br />
+                <code>{`<CountrySelector />`}</code>
               </li>
               <li>
                 <Badge color="purple-glycine">CustomBreadcrumb</Badge>&nbsp;: fil d'Ariane personnalisé pour la navigation.
               </li>
               <li>
                 <Badge color="purple-glycine">FieldsMainCard</Badge>&nbsp;: carte d'affichage des filières principales.
+                <br />
+                <FieldsMainCard
+                  descriptionNode={<Badge color="yellow-tournesol">description ...</Badge>}
+                  number={12}
+                  label="Nombre de filières représentées sur le territoire"
+                  to={`/atlas/effectifs-par-filiere}`}
+                />
+                <br />
+                <code>
+                  {`<FieldsMainCard
+  descriptionNode={<Badge color="yellow-tournesol">description ...</Badge>}
+  number={12}
+  label="Nombre de filières représentées sur le territoire"
+  to={/atlas/effectifs-par-filiere}
+/>`}
+                </code>
               </li>
               <li>
                 <Badge color="purple-glycine">FilieresList</Badge>&nbsp;: liste des filières avec recherche et filtrage.
