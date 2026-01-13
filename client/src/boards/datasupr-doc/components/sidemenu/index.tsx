ea9b4d295013308a97d3c5c9465f53296335b143
jerem <jeremy.peglion@gmail.com>
2026-01-13 09:31:16 +0100

    feat(sidemenu): remplacer le menu latéral par un composant sommaire

--- client/src/boards/datasupr-doc/components/sidemenu/index.tsx
+++ client/src/boards/datasupr-doc/components/sidemenu/index.tsx
@@ -1,46 +1,18 @@
-import { useLocation } from "react-router-dom";
+import { Summary, SummaryItem, SummaryWrapper } from "../../../../components/summary";
 
 import "./styles.scss";
 
 export default function CustomSideMenu() {
-  const location = useLocation();
-
   return (
-    <div title="" className="sidemenu sticky">
-      <div>
-        <ul>
-          <li>
-            <a href="#id1" className={`${location.hash === "#id1" ? "selected" : ""}`}>
-              Gestion des URLs
-            </a>
-          </li>
-          <li>
-            <a href="#id2" className={`${location.hash === "#id2" ? "selected" : ""}`}>
-              Liste des ids
-            </a>
-          </li>
-          <li>
-            <a href="#id3" className={`${location.hash === "#id3" ? "selected" : ""}`}>
-              Ajout d'un graphique
-            </a>
-          </li>
-          <li>
-            <a href="#id4" className={`${location.hash === "#id4" ? "selected" : ""}`}>
-              Liste des composants
-            </a>
-          </li>
-          <li>
-            <a href="#id5" className={`${location.hash === "#id5" ? "selected" : ""}`}>
-              Serveur
-            </a>
-          </li>
-          <li>
-            <a href="#id6" className={`${location.hash === "#id6" ? "selected" : ""}`}>
-              Gestion des CSS
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <SummaryWrapper className="sticky-summary">
+      <Summary title="Sommaire" className="sticky-summary">
+        <SummaryItem href="#id1" label="Gestion des URLs" />
+        <SummaryItem href="#id2" label="Liste des ids" />
+        <SummaryItem href="#id3" label="Ajout d'un graphique" />
+        <SummaryItem href="#id4" label="Liste des composants" />
+        <SummaryItem href="#id5" label="Serveur" />
+        <SummaryItem href="#id6" label="Gestion des CSS" />
+      </Summary>
+    </SummaryWrapper>
   );
 }
