e7a6254f3ab00dfa3524298d8fd49af717eaf262
Anne L'Hôte <anne.lhote@gmail.com>
2026-01-14 12:00:00 +0100

    fix(fundings): Delete unused folders and files

--- client/src/boards/fundings/routes.tsx
+++ client/src/boards/fundings/routes.tsx
@@ -2,13 +2,7 @@ import { Navigate, Route, Routes, useSearchParams } from "react-router-dom";
 
 import { useTitle } from "../../hooks/usePageTitle.tsx";
 import GlobalLayout from "./components/layouts/global-layout.tsx";
-import Counties from "./pages/counties";
 import Home from "./pages/home";
-import SidemenuLaboratories from "./pages/laboratories/components/sidemenu";
-import Laboratories from "./pages/laboratories/index.tsx";
-import National from "./pages/national";
-import SidemenuCounties from "./pages/counties/components/sidemenu";
-import SidemenuNational from "./pages/national/components/sidemenu";
 import SidemenuStructures from "./pages/structures/components/sidemenu";
 import Structures from "./pages/structures/index.tsx";
 import i18n from "./title-i18n.json";
@@ -38,17 +32,8 @@ export default function FundingsRoutes() {
       <Route element={<GlobalLayout languageSelector />}>
         <Route index element={<Navigate to="home" replace />} />
         <Route path="home" element={<RouteWithTitle titleKey="home" element={<Home />} />} />
-        <Route element={<SidemenuNational />}>
-          <Route path="national" element={<RouteWithTitle titleKey="national" element={<National />} />} />
-        </Route>
         <Route element={<SidemenuStructures />}>
           <Route path="structures" element={<RouteWithTitle titleKey="structures" element={<Structures />} />} />
-        </Route>
-        <Route element={<SidemenuLaboratories />}>
-          <Route path="laboratories" element={<RouteWithTitle titleKey="laboratories" element={<Laboratories />} />} />
-        </Route>
-        <Route element={<SidemenuCounties />}>
-          <Route path="counties" element={<RouteWithTitle titleKey="counties" element={<Counties />} />} />
         </Route>
       </Route>
     </Routes>
