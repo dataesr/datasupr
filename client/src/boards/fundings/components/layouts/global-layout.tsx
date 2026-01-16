322908d842ec9c1755d55ab1d79fff30c852ad24
Anne L'Hôte <anne.lhote@gmail.com>
2026-01-16 15:12:34 +0100

    fix(fundings): Correct navigation menu

--- client/src/boards/fundings/components/layouts/global-layout.tsx
+++ client/src/boards/fundings/components/layouts/global-layout.tsx
@@ -1,17 +1,16 @@
-import { Button, Container, FastAccess, Header, Link, Logo, Nav, Service } from "@dataesr/dsfr-plus";
-import { useEffect, useState } from "react";
-import { Outlet, useLocation, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, Outlet, useLocation, useSearchParams } from "react-router-dom";
 
-import SwitchTheme from "../../../../components/switch-theme";
 import Footer from "./footer";
 import i18n from "./i18n.json";
+
 import "../styles.scss";
 
-export default function GlobalLayout({ languageSelector = false }) {
+
+export default function GlobalLayout() {
   const { pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const currentLang = searchParams.get("language") || "fr";
-  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     if (!searchParams.get("year")) {
@@ -29,111 +28,90 @@ export default function GlobalLayout({ languageSelector = false }) {
 
   return (
     <>
-      <Header>
-        <Logo text="Ministère|de l'Enseignement|supérieur,|de la Recherche|et de l'Espace" />
-        <Service name="DataSupR" tagline={getI18nLabel("tagline")} />
-        <FastAccess>
-          <Button as="a" href="/" icon="github-fill" size="sm" variant="text">
-            {getI18nLabel("explore")}
-          </Button>
-          <Button
-            as="a"
-            href="https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-atlas_regional-effectifs-d-etudiants-inscrits/table/?disjunctive.rgp_formations_ou_etablissements&sort=-rentree"
-            icon="code-s-slash-line"
-            rel="noreferer noopener"
-            size="sm"
-            target="_blank"
-            variant="text"
-          >
-            {getI18nLabel("datasets")}
-          </Button>
-          <Button aria-controls="fr-theme-modal" className="fr-btn fr-icon-theme-fill" data-fr-opened="false">
-            {getI18nLabel("themes")}
-          </Button>
-          {languageSelector && (
-            <nav role="navigation" className="fr-translate fr-nav">
-              <div className="fr-nav__item">
-                <button
-                  aria-controls="translate-1177"
-                  aria-expanded="false"
-                  className="fr-translate__btn fr-btn fr-btn--tertiary"
-                  title={getI18nLabel("languagesSelector")}
-                >
-                  {currentLang === "fr" ? (
-                    <>
-                      FR<span className="fr-hidden-lg"> - Français</span>
-                    </>
-                  ) : (
-                    <>
-                      EN<span className="fr-hidden-lg"> - English</span>
-                    </>
-                  )}
-                </button>
-                <div className="fr-collapse fr-translate__menu fr-menu" id="translate-1177">
-                  <ul className="fr-menu__list">
-                    <li>
-                      <Button
-                        aria-current={searchParams.get("language") === "fr"}
-                        className="fr-translate__language fr-nav__link"
-                        onClick={() => {
-                          searchParams.set("language", "fr");
-                          setSearchParams(searchParams);
-                        }}
-                      >
-                        FR - Français
-                      </Button>
-                    </li>
-                    <li>
-                      <Button
-                        aria-current={searchParams.get("language") === "en"}
-                        className="fr-translate__language fr-nav__link"
-                        onClick={() => {
-                          searchParams.set("language", "en");
-                          setSearchParams(searchParams);
-                        }}
-                      >
-                        EN - English
-                      </Button>
-                    </li>
-                  </ul>
+      <header role="banner" className="fr-header">
+        <div className="fr-header__body">
+          <div className="fr-container">
+            <div className="fr-header__body-row">
+              <div className="fr-header__brand fr-enlarge-link">
+                <div className="fr-header__brand-top">
+                  <div className="fr-header__logo">
+                    <a href="/" title="Accueil - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)">
+                      <p className="fr-logo">
+                        Ministère
+                        <br />
+                        de l'Enseignement
+                        <br />
+                        supérieur,
+                        <br />
+                        de la Recherche
+                        <br />
+                        et de l'Espace
+                      </p>
+                    </a>
+                  </div>
+                  <div className="fr-header__navbar">
+                    <button
+                      data-fr-opened="false"
+                      aria-controls="modal-header"
+                      title="Menu"
+                      type="button"
+                      id="button-header"
+                      className="fr-btn--menu fr-btn"
+                    >
+                      Menu
+                    </button>
+                  </div>
                 </div>
               </div>
-            </nav>
-          )}
-        </FastAccess>
-      </Header>
-      <div className="fd-main-menu">
-        <Container>
-          <div className="actions">
-            <button
-              className="fd-menu-toggle fr-btn fr-btn--tertiary"
-              onClick={() => setMenuOpen(!menuOpen)}
-              aria-expanded={menuOpen}
-              aria-controls="fd-main-nav"
-            >
-              {menuOpen ? <i className="ri-close-line" /> : <i className="ri-menu-line" />}
-              Menu
-            </button>
-            <div className={`fd-nav-wrapper ${menuOpen ? "fd-nav-open" : ""}`} id="fd-main-nav">
-              <Nav aria-label="Main navigation">
-                <Link current={is("/fundings/home")} href="/fundings/home">
-                  <span className="fr-icon-home-4-line fr-mr-1w" aria-hidden="true" />
-                  {getI18nLabel("home")}
-                </Link>
-                <Link current={is("/fundings/structures")} href={`/fundings/structures?${searchParams}`}>
-                  {getI18nLabel("structures")}
-                </Link>
-                <Link current={is("/fundings/comparison")} href={`/fundings/comparison?${searchParams}`}>
-                  {getI18nLabel("comparison")}
-                </Link>
-              </Nav>
             </div>
           </div>
-        </Container>
-      </div>
+        </div>
+        <div className="fr-header__menu fr-modal" id="modal-header" aria-labelledby="button-header">
+          <div className="fr-container">
+            <button aria-controls="modal-header" title="Fermer" type="button" id="button-2168" className="fr-btn--close fr-btn">
+              {getI18nLabel("close")}
+            </button>
+            <div className="fr-header__menu-links"></div>
+            <nav className="fr-nav" role="navigation" aria-label="Menu principal">
+              <ul className="fr-nav__list">
+                <li className="fr-nav__item">
+                  <Link
+                    to="/fundings/home"
+                    target="_self"
+                    {...(pathname === "/fundings/home" && { "aria-current": "page" })}
+                    className="fr-nav__link"
+                  >
+                    <span className="fr-icon-home-4-line fr-mr-1w" aria-hidden="true" />
+                    {getI18nLabel("home")}
+                  </Link>
+                </li>
+                <li className="fr-nav__item">
+                  <Link
+                    to={`/fundings/structures?${searchParams}`}
+                    target="_self"
+                    {...(is("/fundings/structures") && { "aria-current": "page" })}
+                    className="fr-nav__link"
+                  >
+                    {getI18nLabel("structures")}
+                  </Link>
+                </li>
+                <li className="fr-nav__item">
+                  <Link
+                    to={`/fundings/comparison?${searchParams}`}
+                    target="_self"
+                    {...(is("/fundings/comparison") && { "aria-current": "page" })}
+                    className="fr-nav__link"
+                  >
+                    {getI18nLabel("comparison")}
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
       <Outlet />
       <Footer />
-      <SwitchTheme />
     </>
   );
 }
