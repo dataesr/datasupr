38c30b4eb0f28e662f2274bb4a354d60241eeb1d
Anne L'Hôte <anne.lhote@gmail.com>
2026-01-16 12:27:31 +0100

    fix(ci/cd): Correct default value

--- client/src/boards/fundings/components/year-selector/index.tsx
+++ client/src/boards/fundings/components/year-selector/index.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "react-router-dom";
 
 export default function YearSelector() {
   const [searchParams, setSearchParams] = useSearchParams({});
-  const year = searchParams.get("year");
+  const year = searchParams.get("year") ?? "";
   const years = Array.from(Array(10).keys()).map((item) => item + 2015);
 
   const handleYearChange = (year: string) => {
