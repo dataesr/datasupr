b348cb0d074a1b5cfdbeca7fe9446003012d3113
Anne L'Hôte <anne.lhote@gmail.com>
2026-01-21 11:33:17 +0100

    fix(ci/cd): Remove unused import

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import AdminRoutes from "./boards/admin/routes.tsx";
 import AtlasRoutes from "./boards/atlas/routes.tsx";
