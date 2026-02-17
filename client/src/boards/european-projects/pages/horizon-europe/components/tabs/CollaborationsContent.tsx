09402bf68e6d59e54d56186271ca3b22baaeec6c
jerem <jeremy.peglion@gmail.com>
2026-02-17 11:39:46 +0100

    feat(i18n): ajouter des labels multilingues pour les composants de collaboration et de synthèse

--- client/src/boards/european-projects/pages/horizon-europe/components/tabs/CollaborationsContent.tsx
+++ client/src/boards/european-projects/pages/horizon-europe/components/tabs/CollaborationsContent.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import { Row, Col, Title } from "@dataesr/dsfr-plus";
 
 import BoardsSuggestComponent from "../../../../../../components/boards-suggest-component";
@@ -7,8 +8,23 @@ import CountriesCollaborationsTable from "../../../collaborations/charts/countri
 import CountryNeighbourgs from "../../../collaborations/charts/country-neighbourgs";
 import EntityVariablePie from "../../../collaborations/charts/entity-variable-pie";
 import MapOfEuropeCollaborationsFlow from "../../../collaborations/charts/map-of-europe-collaborations-flow";
+import { getI18nLabel } from "../../../../../../utils";
+
+const i18n = {
+  "collaborations-by-entity-title": {
+    fr: "Répartition des collaborations par entité",
+    en: "Collaboration distribution by entity",
+  },
+  "collaborations-by-entity-callout": {
+    fr: "Visualisez la répartition des collaborations du pilier/programme/thématique/destination sélectionné(e) par entité. Vous pouvez rechercher une entité spécifique pour voir ses collaborations. Par exemple, si vous recherchez une université, vous verrez la part des collaborations impliquant cette université par rapport au total des collaborations du pilier/programme/thématique/destination. Toujours dans l'écosystème du pays sélectionné. Cela vous permet d'identifier les entités les plus actives et de comprendre leur rôle dans les collaborations au sein du pays. Attention, seules les entités du pays apparaissent dans la recherche.",
+    en: "Visualize the collaboration distribution of the selected pillar/program/topic/destination by entity. You can search for a specific entity to see its collaborations. For example, if you search for a university, you will see the share of collaborations involving that university compared to the total collaborations of the pillar/program/topic/destination. Always within the selected country's ecosystem. This allows you to identify the most active entities and understand their role in collaborations within the country. Note that only entities from the country appear in the search.",
+  },
+};
 
 export default function CollaborationsContent() {
+  const [searchParams] = useSearchParams();
+  const currentLang = searchParams.get("language") || "fr";
+
   return (
     <div>
       <Row>
@@ -32,16 +48,9 @@ export default function CollaborationsContent() {
       <Row className="fr-mt-5w">
         <Col>
           <Title as="h2" look="h4">
-            Répartition des collaborations par entité
+            {getI18nLabel(i18n, "collaborations-by-entity-title", currentLang)}
           </Title>
-          <Callout className="callout-style">
-            Visualisez la répartition des collaborations du pilier/programme/thématique/destination sélectionné(e) par entité.
-            <br /> Vous pouvez rechercher une entité spécifique pour voir ses collaborations.
-            <br /> Par exemple, si vous recherchez une université, vous verrez la part des collaborations impliquant cette université par rapport au
-            total des collaborations du pilier/programme/thématique/destination. Toujours dans l'écosystème du pays sélectionné.
-            <br /> Cela vous permet d'identifier les entités les plus actives et de comprendre leur rôle dans les collaborations au sein du pays.
-            <br /> Attention, seules les entités du pays apparaissent dans la recherche.
-          </Callout>
+          <Callout className="callout-style">{getI18nLabel(i18n, "collaborations-by-entity-callout", currentLang)}</Callout>
         </Col>
       </Row>
       <Row>
--- client/src/boards/european-projects/pages/horizon-europe/components/tabs/SyntheseContent.tsx
+++ client/src/boards/european-projects/pages/horizon-europe/components/tabs/SyntheseContent.tsx
@@ -14,7 +14,37 @@ import SynthesisFocus from "../../../overview/charts/synthesis-focus";
 import Callout from "../../../../../../components/callout";
 import PillarsFundingEvo3Years from "../../../overview/charts/pillars-funding-evo-3-years";
 import BoardsSuggestComponent from "../../../../../../components/boards-suggest-component";
+import { getI18nLabel } from "../../../../../../utils";
 // import DestinationsFunding from "../../../overview/components/destinations-funding";
+
+const i18n = {
+  "pillar-comparison-title": { fr: "Comparaison de la répartition des subventions par pilier", en: "Comparison of grant distribution by pillar" },
+  "pillar-detail-title": { fr: "Détails du pilier sur les 3 dernières années", en: "Pillar details over the last 3 years" },
+  "pillar-detail-callout": {
+    fr: "Visualisez l'évolution des subventions demandées et obtenues pour le pilier sélectionné sur les trois dernières années du programme Horizon Europe. Cela vous permet d'analyser les tendances de financement et d'évaluer la performance du pilier au fil du temps. Vous pouvez également ajuster l'affichage pour visualister le total des subventions, le nombre total de coordinations ou le nombre total de participations.",
+    en: "Visualize the evolution of requested and obtained grants for the selected pillar over the last three years of the Horizon Europe program. This allows you to analyze funding trends and evaluate the pillar's performance over time. You can also adjust the display to view total grants, total coordinations, or total participations.",
+  },
+  "pillar-composition": { fr: "Composition du pilier", en: "Pillar composition" },
+  "pillar-composition-callout": {
+    fr: "Visualisez la répartition des subventions du pilier sélectionné par programme. Cela vous permet d'identifier les programmes les plus financés et de comprendre leur contribution au sein du pilier.",
+    en: "Visualize the grant distribution of the selected pillar by program. This allows you to identify the most funded programs and understand their contribution within the pillar.",
+  },
+  "grant-distribution-by-program": { fr: "Répartition des subventions par programme", en: "Grant distribution by program" },
+  "main-beneficiaries": { fr: "Principaux bénéficiaires", en: "Main beneficiaries" },
+  "program-composition": { fr: "Composition du programme", en: "Program composition" },
+  "program-composition-callout": {
+    fr: "Visualisez la répartition des subventions du programme sélectionné par thématique. Cela vous permet d'identifier les thématiques les plus financées et de comprendre leur contribution au sein du programme.",
+    en: "Visualize the grant distribution of the selected program by topic. This allows you to identify the most funded topics and understand their contribution within the program.",
+  },
+  "grant-distribution-by-topic": { fr: "Répartition des subventions par thématique", en: "Grant distribution by topic" },
+  "thematic-composition": { fr: "Composition du ou des thématiques", en: "Thematic composition" },
+  "thematic-composition-callout": {
+    fr: "Visualisez la répartition des subventions de la ou des thématiques sélectionnée(s) par destination. Cela vous permet d'identifier les destinations les plus financées et de comprendre leur contribution au sein de la ou des thématiques.",
+    en: "Visualize the grant distribution of the selected topic(s) by destination. This allows you to identify the most funded destinations and understand their contribution within the topic(s).",
+  },
+  "grant-distribution-by-destination": { fr: "Répartition des subventions par destination", en: "Grant distribution by destination" },
+  "no-content": { fr: "Aucun contenu disponible", en: "No content available" },
+};
 
 // export default function SyntheseContent({ contentType }: SyntheseContentProps) {
 export default function SyntheseContent() {
@@ -24,6 +54,7 @@ export default function SyntheseContent() {
   const programId = searchParams.get("programId");
   const thematicIds = searchParams.get("thematicIds");
   const destinationIds = searchParams.get("destinationIds");
+  const currentLang = searchParams.get("language") || "fr";
 
   let contentType = "pillar-comparison";
   if (pillarId && programId && thematicIds && destinationIds) {
@@ -42,12 +73,9 @@ export default function SyntheseContent() {
         <div className="fr-pb-3w">
           <SynthesisFocus />
           <Title as="h2" className="fr-mt-5w">
-            Comparaison de la répartition des subventions par pilier
+            {getI18nLabel(i18n, "pillar-comparison-title", currentLang)}
           </Title>
           <PillarsFunding />
-          <Title as="h2" className="fr-mt-5w">
-            Principaux bénéficiaires
-          </Title>
           <MainPartners />
         </div>
       );
@@ -65,29 +93,17 @@ export default function SyntheseContent() {
               <PillarsOverview />
             </Col>
           </Row>
-          <Title as="h2" className="fr-mt-5w">
-            Principaux bénéficiaires
-          </Title>
           <MainPartners />
           <Title as="h2" className="fr-mt-5w">
-            Détails du pilier sur les 3 dernières années
+            {getI18nLabel(i18n, "pillar-detail-title", currentLang)}
           </Title>
-          <Callout className="callout-style">
-            Visualisez l'évolution des subventions demandées et obtenues pour le pilier sélectionné sur les trois dernières années du programme
-            Horizon Europe.
-            <br /> Cela vous permet d'analyser les tendances de financement et d'évaluer la performance du pilier au fil du temps.
-            <br /> Vous pouvez également ajuster l'affichage pour visualister le total des subventions, le nombre total de coordinations ou le nombre
-            total de participations.
-          </Callout>
+          <Callout className="callout-style">{getI18nLabel(i18n, "pillar-detail-callout", currentLang)}</Callout>
           <PillarsFundingEvo3Years />
           <Title as="h2" className="fr-mt-5w">
-            Composition du pilier
+            {getI18nLabel(i18n, "pillar-composition", currentLang)}
           </Title>
-          <Callout className="callout-style">
-            Visualisez la répartition des subventions du pilier sélectionné par programme.
-            <br /> Cela vous permet d'identifier les programmes les plus financés et de comprendre leur contribution au sein du pilier.
-          </Callout>
-          <Title as="h3">Répartition des subventions par programme</Title>
+          <Callout className="callout-style">{getI18nLabel(i18n, "pillar-composition-callout", currentLang)}</Callout>
+          <Title as="h3">{getI18nLabel(i18n, "grant-distribution-by-program", currentLang)}</Title>
           <ProgramsFunding />
           <BoardsSuggestComponent />
         </Container>
@@ -99,17 +115,14 @@ export default function SyntheseContent() {
           <SynthesisFocus />
           <ProgramsOverview />
           <Title as="h2" className="fr-mt-5w">
-            Principaux bénéficiaires
+            {getI18nLabel(i18n, "main-beneficiaries", currentLang)}
           </Title>
           <MainPartners />
           <Title as="h2" className="fr-mt-5w">
-            Composition du programme
+            {getI18nLabel(i18n, "program-composition", currentLang)}
           </Title>
-          <Callout className="callout-style">
-            Visualisez la répartition des subventions du programme sélectionné par thématique.
-            <br /> Cela vous permet d'identifier les thématiques les plus financées et de comprendre leur contribution au sein du programme.
-          </Callout>
-          <Title as="h3">Répartition des subventions par thématique</Title>
+          <Callout className="callout-style">{getI18nLabel(i18n, "program-composition-callout", currentLang)}</Callout>
+          <Title as="h3">{getI18nLabel(i18n, "grant-distribution-by-topic", currentLang)}</Title>
           <TopicsFunding />
           <BoardsSuggestComponent />
         </Container>
@@ -121,18 +134,14 @@ export default function SyntheseContent() {
           <SynthesisFocus />
           <ThematicsOverview />
           <Title as="h2" className="fr-mt-5w">
-            Principaux bénéficiaires
+            {getI18nLabel(i18n, "main-beneficiaries", currentLang)}
           </Title>
           <MainPartners />
           <Title as="h2" className="fr-mt-5w">
-            Composition du ou des thématiques
+            {getI18nLabel(i18n, "thematic-composition", currentLang)}
           </Title>
-          <Callout className="callout-style">
-            Visualisez la répartition des subventions de la ou des thématiques sélectionnée(s) par destination.
-            <br /> Cela vous permet d'identifier les destinations les plus financées et de comprendre leur contribution au sein de la ou des
-            thématiques.
-          </Callout>
-          <Title as="h3">Répartition des subventions par destination</Title>
+          <Callout className="callout-style">{getI18nLabel(i18n, "thematic-composition-callout", currentLang)}</Callout>
+          <Title as="h3">{getI18nLabel(i18n, "grant-distribution-by-destination", currentLang)}</Title>
           <DestinationsFunding />
           <BoardsSuggestComponent />
         </Container>
@@ -144,7 +153,7 @@ export default function SyntheseContent() {
           <SynthesisFocus />
           <DestinationsOverview />
           <Title as="h2" className="fr-mt-5w">
-            Principaux bénéficiaires
+            {getI18nLabel(i18n, "main-beneficiaries", currentLang)}
           </Title>
           <MainPartners />
           <BoardsSuggestComponent />
@@ -152,6 +161,6 @@ export default function SyntheseContent() {
       );
 
     default:
-      return <div>Aucun contenu disponible</div>;
+      return <div>{getI18nLabel(i18n, "no-content", currentLang)}</div>;
   }
 }
