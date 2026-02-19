a3497291e63d5539adfadf8d8fe7ff0fb7ba0b6b
eric <eric.jeangirard@recherche.gouv.fr>
2026-02-19 17:08:42 +0100

    fix(fundigs): wording home

--- client/src/boards/financements-par-aap/pages/home/index.tsx
+++ client/src/boards/financements-par-aap/pages/home/index.tsx
@@ -50,24 +50,21 @@ export default function Home() {
             Les informations présentées reposent sur trois grandes catégories de sources de
             financements.
             <br/> <strong>Financements ANR</strong> : 
-            L'Agence Nationale de la Recherche (ANR) expose en open data deux jeux de données 
-            (DGDS pour les projets ANR et DGPIE pour les projets PIA ANR). Ces sources fournissent 
+            l'Agence Nationale de la Recherche (ANR) expose en open data <a target='_blank' href='https://www.data.gouv.fr/organizations/agence-nationale-de-la-recherche/datasets'>deux jeux de données</a> : DGDS pour les projets ANR et DGPIE pour les projets du Programme d’investissements d’avenir gérés par l’ANR. Ces sources fournissent 
             des informations détaillées sur les projets financés par l’ANR, avec une description des 
             participants généralement disponible au niveau des laboratoires pour les projets DGDS.
             
-            <br/> <strong>Financements PIA</strong> : 
-            Les informations concernant les projets du Programme d’Investissements d’Avenir (PIA) sont issues des données affichées sur Piaweb. Elles sont
-            principalement disponibles à un niveau institutionnel. Pour les financements PIA ANR, les données sont croisées avec la source DGPIE ci-dessus.
-            
+           <br/> <strong>Financements PIA</strong> : 
+          les informations concernant les projets du Programme d’Investissements d’Avenir (PIA) sont issues des données affichées sur Piaweb. Elles sont
+            principalement disponibles à un niveau institutionnel. Pour les financements PIA ANR, les données sont croisées avec le jeu de données ANR-DGPIE mentionné ci-dessus.
             <br/> <strong>Financements européens</strong> : 
-            Les données relatives aux programmes <strong>Horizon Europe</strong> et{' '}
-            <strong>Horizon 2020</strong> sont issues du jeu de données open data{' '}
-            <strong>PCRI</strong>.
-            Elles décrivent principalement les participants au niveau institutionnel. Un
-            travail complémentaire est mené avec le CNRS, le CEA, l’INRAE, l’INRIA et l’ONERA afin
+            les données relatives aux programmes <strong>Horizon Europe</strong> 2021-2027 et{' '}
+            <strong>Horizon 2020</strong> 2014-2020 sont issues des <a target='_blank' href='https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-horizon-projects-entities/information/?disjunctive.paysage_category&disjunctive.free_keywords'>jeux de données open data</a>{' '}
+             produits par le MESRE sur les projets financés et les partenaires, à partir de la base eCorda sur les programmes cadres de recherche et d’innovation de l’Union européenne.
+            Ces jeux de données permettent d’identifier les participants au niveau institutionnel. Par rapport aux données diffusées en opendata par la Commission, ils apportent une information précise sur les laboratoires publics qui conduisent, en France, les projets financés. Cet enrichissement est le fruit d’ un
+            travail complémentaire mené en collaboration avec le CNRS, le CEA, INRAE, Inria et l’ONERA afin
             d’enrichir ces données et de reconstituer, lorsque cela est possible, le niveau
-            laboratoire pour les financements européens. Horizon 2020 couvre la période 2014–2020,
-            tandis que son successeur, Horizon Europe couvre 2021-2027.
+            laboratoire pour les financements européens.
           </Text>
           <Title as="h2" look="h5">
             ⚠️ Financement global / Financement perçu
@@ -76,29 +73,29 @@ export default function Home() {
           Trois modes de comptabilisation des financements sont exposés.
           <br/> <strong>Nombre de projets financés (participation)</strong> : on compte le nombre de projets pour lesquels l’établissement est partenaire, indépendamment des financements associés.
           <br/> <strong>Financement global (volume)</strong> : on compte le financement global du projet, c'est-à-dire le montant total attribué, en l'affectant à chaque établissement participant. Cela ne correspond pas aux financements effectivement perçus par l'établissement. Cela représente le volume total de financements des projets auxquels participe l'établissement. Cet indicateur permet de traduire l'envergure des projets auxquels l'établissement participe.
-          <br/> <strong>Financement perçu (implication)</strong> : on compte la part réelle allouée à chaque établissement partenaire d’un projet (colonne « Projet.Partenaire.Aide_allouee.ANR » dans la base ANR, « montant des subvention » dans la base Horizon Europe, « consommation » dans la base PIA. Attention, on mélange ici de la consommation (pour le PIA) avec des subventions allouées (pour l'ANR et Europe). Les consommations sont affectées à l'année d'édition du projet, et pas à l'année de consommation (il peut y avoir un décalage de plusieurs années). Cela reste une <strong>approximation</strong> de l'<strong>implication</strong> de l'établissement.
+          <br/> <strong>Financement perçu (implication)</strong> : on compte la part réelle allouée à chaque établissement partenaire d’un projet (colonne « Projet.Partenaire.Aide_allouee.ANR » dans la base ANR, « montant des subventions » dans la base Horizon Europe, « consommation » dans la base PIA. Attention, on mélange ici de la consommation (pour le PIA) avec des subventions allouées (pour l'ANR et Europe). Les consommations sont affectées à l'année d'édition du projet, et pas à l'année de consommation (il peut y avoir un décalage de plusieurs années). Cela reste une <strong>approximation</strong> de l'<strong>implication</strong> de l'établissement.
 <br/><em>NB : Il ne s'agit pas de compte fractionnaire, aucun calcul de répartition n'est effectué.</em> 
           </Text>
           <Title as="h2" look="h5">
-            🏛️ Identification des partenaires
+            🏛️ Identification des établissements partenaires
           </Title>
           <Text>
-             Les <strong>référentiels de structures (RNSR)</strong> et d'<strong>établissements 
-            (Paysage)</strong> permettent de rattacher chaque participation observée aux
+             Les <strong>référentiels de structures (<a target='_blank' href='https://rnsr.adc.education.fr/'>Répertoire national des structures de recherche - RNSR)</a></strong> et d'<strong>établissements 
+            (l’application en accès restreint <a target='_blank' href='https://paysage.esr.gouv.fr/'>Paysage</a>)</strong> permettent de rattacher chaque participation observée aux
             structures parentes (tutelles). 
           <br/> Pour l'<strong>ANR</strong> les données des 
             participants sont généralement disponibles au niveau des laboratoires pour les projets DGDS (c'est-à-dire hors PIA), et sont affectées à toutes les tutelles grâce au RNSR.
           <br/>Pour les <strong> financements européens</strong> les participants sont disponibles au niveau institutionnel pour les institutions d'accueil (Host Institution). Un
-            travail complémentaire est mené avec le CNRS, le CEA, l’INRAE, l’INRIA et l’ONERA afin
+            travail complémentaire est mené avec le CNRS, le CEA, INRAE, Inria et l’ONERA afin
             d’enrichir ces données et de reconstituer, lorsque cela est possible, le niveau
-            laboratoire pour les financements européens. Cela permet d'affecter aussi l'information disponibles aux autres tutelles de ces laboratoires : ainsi, pour les institutions qui ne sont pas institutions d'accueil, l'information de présence et d'implication est quand même disponible grâce à ce travail, en passant par le niveau laboratoire. Cet enrichissement mené en collaboration avec ces cinq ONR est réalisé avec un délai d'environ un an. Il n'est donc pas disponible pour les projets les plus récents.
+            laboratoire pour les financements européens. Cela permet d'affecter aussi l'information disponibles aux autres tutelles de ces laboratoires : ainsi, pour les institutions qui ne sont pas institutions d'accueil, l'information de présence et d'implication est quand même disponible grâce à ce travail, en passant par le niveau laboratoire. Cet enrichissement mené en collaboration avec ces cinq organismes nationaux de recherche est réalisé avec un délai d'environ un an (par rapport à la mise à disposition des données). Il n'est donc pas disponible pour les projets les plus récents.
           </Text>
           <Title as="h2" look="h5">
             🧭 Identification des coordinateurs
           </Title>
           <Text>
+Pour les <strong>financements européens</strong>, dans l'objectif de simplifier la lecture des données en ayant une variable binaire Coordinateur Oui/Non, les partenaires de type PI (Principal Investigator) ou Co-PI sont considérés comme coordinateur (notamment le cas pour les ERC). 
 Le rôle de coordinateur est indiqué dans chaque source, donc parfois au niveau institution, parfois au niveau laboratoire. Quand c'est le cas au niveau laboratoire, toutes les tutelles sont aussi considérées comme ayant un rôle de coordinateur.
-Pour les <strong>financements européens</strong>, les partenaires de type PI (Principal Investigator) ou Co-PI sont considérés comme coordinateur (notamment le cas pour les ERC). 
          </Text>
         </Col>
       </Row>
