15ab827568577f404be4e1602c2b24647c74ec1f
Mihoub Debache <debache.mihoub@gmail.com>
2026-03-10 14:00:07 +0100

    fix(structures-finance):add titlekey to nav

--- client/src/boards/structures-finance/routes.tsx
+++ client/src/boards/structures-finance/routes.tsx
@@ -9,7 +9,12 @@ import NationalView from "./pages/national/index.tsx";
 import StructuresView from "./pages/structures/index.tsx";
 
 import "./styles.scss";
+import { useTitle } from "../../hooks/usePageTitle.tsx";
 
+const RouteWithTitle = ({ titleKey, element }) => {
+  useTitle(titleKey);
+  return element;
+};
 
 export default function StructuresFinanceRoutes() {
   return (
@@ -24,11 +29,52 @@ export default function StructuresFinanceRoutes() {
             />
           }
         />
-        <Route path="accueil" element={<AccueilView />} />
-        <Route path="definitions" element={<DefinitionsView />} />
-        <Route path="etablissements" element={<StructuresView />} />
-        <Route path="faq" element={<FAQView />} />
-        <Route path="national" element={<NationalView />} />
+        <Route
+          path="accueil"
+          element={
+            <RouteWithTitle
+              titleKey="Accueil - Financement des établissements d'enseignement supérieur français"
+              element={<AccueilView />}
+            />
+          }
+        />
+        <Route
+          path="definitions"
+          element={
+            <RouteWithTitle
+              titleKey="Definitions - Financement des établissements d'enseignement supérieur français"
+              element={<DefinitionsView />}
+            />
+          }
+        />
+        <Route
+          path="etablissements"
+          element={
+            <RouteWithTitle
+              titleKey="Vue par établissement - Financement des établissements d'enseignement supérieur français
+"
+              element={<StructuresView />}
+            />
+          }
+        />
+        <Route
+          path="faq"
+          element={
+            <RouteWithTitle
+              titleKey="FAQ - Financement des établissements d'enseignement supérieur français"
+              element={<FAQView />}
+            />
+          }
+        />
+        <Route
+          path="national"
+          element={
+            <RouteWithTitle
+              titleKey="Vue nationale - Financement des établissements d'enseignement supérieur français"
+              element={<NationalView />}
+            />
+          }
+        />
       </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
