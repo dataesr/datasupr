512d890616177f84a5b16e379f70b7331c5290e6
Anne L'Hôte <anne.lhote@gmail.com>
2026-03-13 17:17:20 +0100

    fix(fundings): Correct download CSV

--- client/src/boards/financements-par-aap/pages/structures/components/data-table/index.tsx
+++ client/src/boards/financements-par-aap/pages/structures/components/data-table/index.tsx
@@ -120,7 +120,7 @@ export default function DataTable({ name }: { name: string | undefined }) {
     // Use the bracket notation to suppress the error "Property 'grid' does not exist on type 'never'."
     if (gridRef?.current?.['grid']) {
       const currentGrid: any = gridRef?.current['grid'];
-      currentGrid.grid.exporting.downloadCSV();
+      currentGrid.exporting.downloadCSV();
     }
   };
 
