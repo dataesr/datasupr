498e00b1b16475f03243f283efcb2e214253eda0
jerem <jeremy.peglion@gmail.com>
2026-04-08 10:11:11 +0200

    feat(ep/routes): Ajout du lazy loading pour les routes

--- client/src/boards/european-projects/routes.tsx
+++ client/src/boards/european-projects/routes.tsx
@@ -1,22 +1,23 @@
+import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 
-import Beneficiaries from "./pages/beneficiaries/index.tsx";
-import Collaborations from "./pages/collaborations/index.tsx";
-import CollaborationsEntity from "./pages/collaborations/index-entity.tsx";
-import Entities from "./pages/entities/index.tsx";
-import ERC from "./pages/erc/index.tsx";
-import EvolutionPcri from "./pages/evolution-pcri/index.tsx";
-import GlobalLayout from "./components/layouts/global-layout.tsx";
-import Home from "./pages/home/index.tsx";
-import HorizonEurope from "./pages/horizon-europe/index.tsx";
-import Informations from "./pages/informations.tsx";
-import NotFoundPage from "../../components/not-found-page.tsx";
-import Overview from "./pages/overview/index.tsx";
-import Positioning from "./pages/positioning/index.tsx";
-import ProjectsTypes from "./pages/projects-types/index.tsx";
-import Search from "./pages/search/index.tsx";
-import SidemenuLayout from "./components/layouts/sidemenu-layout.tsx";
-import TypeOfBeneficiaries from "./pages/type-of-beneficiaries/index.tsx";
+const Beneficiaries = lazy(() => import("./pages/beneficiaries/index.tsx"));
+const Collaborations = lazy(() => import("./pages/collaborations/index.tsx"));
+const CollaborationsEntity = lazy(() => import("./pages/collaborations/index-entity.tsx"));
+const Entities = lazy(() => import("./pages/entities/index.tsx"));
+const ERC = lazy(() => import("./pages/erc/index.tsx"));
+const EvolutionPcri = lazy(() => import("./pages/evolution-pcri/index.tsx"));
+const GlobalLayout = lazy(() => import("./components/layouts/global-layout.tsx"));
+const Home = lazy(() => import("./pages/home/index.tsx"));
+const HorizonEurope = lazy(() => import("./pages/horizon-europe/index.tsx"));
+const Informations = lazy(() => import("./pages/informations.tsx"));
+const NotFoundPage = lazy(() => import("../../components/not-found-page.tsx"));
+const Overview = lazy(() => import("./pages/overview/index.tsx"));
+const Positioning = lazy(() => import("./pages/positioning/index.tsx"));
+const ProjectsTypes = lazy(() => import("./pages/projects-types/index.tsx"));
+const Search = lazy(() => import("./pages/search/index.tsx"));
+const SidemenuLayout = lazy(() => import("./components/layouts/sidemenu-layout.tsx"));
+const TypeOfBeneficiaries = lazy(() => import("./pages/type-of-beneficiaries/index.tsx"));
 
 import { useTitle } from "../../hooks/usePageTitle.tsx";
 import { getI18nLabel } from "../../utils";
@@ -38,31 +39,212 @@ const EntityWrapper = () => {
 export default function EuropeanProjectsRoutes() {
   return (
     <Routes>
-      <Route element={<GlobalLayout />}>
+      <Route
+        element={
+          <Suspense>
+            <GlobalLayout />
+          </Suspense>
+        }
+      >
         <Route index element={<Navigate to="accueil" replace />} />
         <Route path="" element={<Navigate to="accueil" replace />} />
-        <Route path="accueil" element={<RouteWithTitle titleKey="accueil" element={<Home />} />} />
-        <Route path="search" element={<RouteWithTitle titleKey="search" element={<Search />} />} />
-        <Route path="horizon-europe" element={<RouteWithTitle titleKey="horizon-europe" element={<HorizonEurope />} />} />
+        <Route
+          path="accueil"
+          element={
+            <RouteWithTitle
+              titleKey="accueil"
+              element={
+                <Suspense>
+                  <Home />
+                </Suspense>
+              }
+            />
+          }
+        />
+        <Route
+          path="search"
+          element={
+            <RouteWithTitle
+              titleKey="search"
+              element={
+                <Suspense>
+                  <Search />
+                </Suspense>
+              }
+            />
+          }
+        />
+        <Route
+          path="horizon-europe"
+          element={
+            <RouteWithTitle
+              titleKey="horizon-europe"
+              element={
+                <Suspense>
+                  <HorizonEurope />
+                </Suspense>
+              }
+            />
+          }
+        />
         <Route element={<SidemenuLayout />}>
-          <Route path="synthese" element={<RouteWithTitle titleKey="synthese" element={<Overview />} />} />
-          <Route path="positionnement" element={<RouteWithTitle titleKey="positionnement" element={<Positioning />} />} />
-          <Route path="collaborations" element={<RouteWithTitle titleKey="collaborations" element={<Collaborations />} />} />
-          <Route path="collaborations/:entityId" element={<RouteWithTitle titleKey="collaborations" element={<EntityWrapper />} />} />
-          <Route path="beneficiaires" element={<RouteWithTitle titleKey="beneficiaires" element={<Beneficiaries />} />} />
-          <Route path="beneficiaires-types" element={<RouteWithTitle titleKey="beneficiaires-types" element={<TypeOfBeneficiaries />} />} />
+          <Route
+            path="synthese"
+            element={
+              <RouteWithTitle
+                titleKey="synthese"
+                element={
+                  <Suspense>
+                    <Overview />
+                  </Suspense>
+                }
+              />
+            }
+          />
+          <Route
+            path="positionnement"
+            element={
+              <RouteWithTitle
+                titleKey="positionnement"
+                element={
+                  <Suspense>
+                    <Positioning />
+                  </Suspense>
+                }
+              />
+            }
+          />
+          <Route
+            path="collaborations"
+            element={
+              <RouteWithTitle
+                titleKey="collaborations"
+                element={
+                  <Suspense>
+                    <Collaborations />
+                  </Suspense>
+                }
+              />
+            }
+          />
+          <Route
+            path="collaborations/:entityId"
+            element={
+              <RouteWithTitle
+                titleKey="collaborations"
+                element={
+                  <Suspense>
+                    <EntityWrapper />
+                  </Suspense>
+                }
+              />
+            }
+          />
+          <Route
+            path="beneficiaires"
+            element={
+              <RouteWithTitle
+                titleKey="beneficiaires"
+                element={
+                  <Suspense>
+                    <Beneficiaries />
+                  </Suspense>
+                }
+              />
+            }
+          />
+          <Route
+            path="beneficiaires-types"
+            element={
+              <RouteWithTitle
+                titleKey="beneficiaires-types"
+                element={
+                  <Suspense>
+                    <TypeOfBeneficiaries />
+                  </Suspense>
+                }
+              />
+            }
+          />
         </Route>
         <Route path="msca" element={<RouteWithTitle titleKey="msca" element={<div>MSCA</div>} />} />
-        <Route path="erc" element={<RouteWithTitle titleKey="erc" element={<ERC />} />} />
-        <Route path="entities" element={<RouteWithTitle titleKey="entities" element={<Entities />} />} />
-        <Route path="objectifs-types-projets" element={<RouteWithTitle titleKey="objectifs-types-projets" element={<ProjectsTypes />} />} />
+        <Route
+          path="erc"
+          element={
+            <RouteWithTitle
+              titleKey="erc"
+              element={
+                <Suspense>
+                  <ERC />
+                </Suspense>
+              }
+            />
+          }
+        />
+        <Route
+          path="entities"
+          element={
+            <RouteWithTitle
+              titleKey="entities"
+              element={
+                <Suspense>
+                  <Entities />
+                </Suspense>
+              }
+            />
+          }
+        />
+        <Route
+          path="objectifs-types-projets"
+          element={
+            <RouteWithTitle
+              titleKey="objectifs-types-projets"
+              element={
+                <Suspense>
+                  <ProjectsTypes />
+                </Suspense>
+              }
+            />
+          }
+        />
         <Route path="programme-mires" element={<RouteWithTitle titleKey="programme-mires" element={<div>Programme MIRES</div>} />} />
         <Route path="appel-a-projets" element={<RouteWithTitle titleKey="appel-a-projets" element={<div>Appel à projets</div>} />} />
         <Route path="donnees-reference" element={<RouteWithTitle titleKey="donnees-reference" element={<div>Données de référence</div>} />} />
-        <Route path="informations" element={<RouteWithTitle titleKey="informations" element={<Informations />} />} />
-        <Route path="evolution-pcri" element={<RouteWithTitle titleKey="evolution-pcri" element={<EvolutionPcri />} />} />
+        <Route
+          path="informations"
+          element={
+            <RouteWithTitle
+              titleKey="informations"
+              element={
+                <Suspense>
+                  <Informations />
+                </Suspense>
+              }
+            />
+          }
+        />
+        <Route
+          path="evolution-pcri"
+          element={
+            <RouteWithTitle
+              titleKey="evolution-pcri"
+              element={
+                <Suspense>
+                  <EvolutionPcri />
+                </Suspense>
+              }
+            />
+          }
+        />
       </Route>
-      <Route path="*" element={<NotFoundPage />} />
+      <Route
+        path="*"
+        element={
+          <Suspense>
+            <NotFoundPage />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
